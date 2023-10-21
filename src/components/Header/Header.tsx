2b34f86619227f0a6b8a5b8c6137a303b07ced5f
roshkoff571 <74923347+roshkoff571@users.noreply.github.com>
2023-10-21 20:44:58 +0300

    apple touch icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import logo from "../../assets/logo.svg";
 export default function Header() {
   return (
     <div>
+      <h4 style={{ color: "#000" }}>Developed by urionzzz</h4>
       <img src={logo} />
     </div>
   );
